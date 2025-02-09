97c0a4ad3ee18336562a0181d3286c04b24a54b9
Sofiia Kalinina <kalininsofiya@gmail.com>
2025-02-09 23:20:15 +0100

    Use path to lambdas relative to environments/main.tf

--- infra/modules/lambdas/main.tf
+++ infra/modules/lambdas/main.tf
@@ -1,6 +1,7 @@
 data "archive_file" "init_session_lambda" {
   type        = "zip"
-  source_file = "init_session.py"
+  source_file = "../../lambdas/init_session.py"
+
   output_path = "init_session.zip"
 }
 
