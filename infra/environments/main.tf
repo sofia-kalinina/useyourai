2016f39752115f574e489f30d40f21beb3b43e1a
Sofiia Kalinina <kalininsofiya@gmail.com>
2025-02-12 00:24:32 +0100

    Delete lambda to recreate

--- infra/environments/main.tf
+++ infra/environments/main.tf
@@ -4,10 +4,10 @@ module "dynamodb" {
   environment = var.environment  
 }
 
-module "lambdas" {
-  source = "../modules/lambdas"
+# module "lambdas" {
+#   source = "../modules/lambdas"
 
-  account_id  = var.account_id
-  environment = var.environment
-  region      = var.region
-}
+#   account_id  = var.account_id
+#   environment = var.environment
+#   region      = var.region
+# }
