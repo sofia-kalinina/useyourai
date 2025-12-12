fcfcc9344ea6f995cbc9c89e23d20aea6fbc5006
Sofia Kalinina <sofiia.ruse@gmail.com>
2025-12-12 21:58:13 +0100

    Add a hosted zone

---
+++ infra/base/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "sofiia-kalinina"
+
+    workspaces {
+      name = "useyourai-base"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.20"
+    }
+  }
+}
