4f32cfa2549ee166b27f3c0a0e4b87a8e627c160
Sofia Kalinina <sofiia.ruse@gmail.com>
2025-12-12 22:10:39 +0100

    Add tags to the base module, delete non used tfvars

--- infra/base/dns.tf
+++ infra/base/dns.tf
@@ -1,3 +1,5 @@
 resource "aws_route53_zone" "primary" {
   name = "useyourai.eu"
+
+  tags = local.common_tags
 }
--- infra/environments/dev/provider.tf
+++ infra/environments/dev/provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "AWS region for provider configuration"
-  type        = string
-  default     = "eu-central-1"
+  region = var.region
 }
